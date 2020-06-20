639a52cda526a4e9d324a54a43b805e8dee4f608
ammama25 <amyazdy@gmail.com>
2020-06-20 04:10:30 -0700

    mail server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,26 +80,37 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Tell Action Mailer to use smtp server, if configured
-  config.action_mailer.delivery_method = ENV['SMTP_SERVER'].present? ? :smtp : :sendmail
+  config.action_mailer.delivery_method = true ? :smtp : :sendmail
 
-  ActionMailer::Base.smtp_settings = if ENV['SMTP_AUTH'].present? && ENV['SMTP_AUTH'] != "none"
-    {
-      address: ENV['SMTP_SERVER'],
-      port: ENV["SMTP_PORT"],
-      domain: ENV['SMTP_DOMAIN'],
-      user_name: ENV['SMTP_USERNAME'],
-      password: ENV['SMTP_PASSWORD'],
-      authentication: ENV['SMTP_AUTH'],
-      enable_starttls_auto: ENV['SMTP_STARTTLS_AUTO'],
-    }
-  else
-    {
-      address: ENV['SMTP_SERVER'],
-      port: ENV["SMTP_PORT"],
-      domain: ENV['SMTP_DOMAIN'],
-      enable_starttls_auto: ENV['SMTP_STARTTLS_AUTO'],
-    }
-  end
+  ActionMailer::Base.smtp_settings = {
+    address: 'mail.dunion.ir',
+    port: '25',
+    domain: 'dunion.ir',
+    user_name: 'noreply@dunion.ir',
+    password: 'o{$p-q{Mu!3v',
+    authentication: :plain ,
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none'
+  }
+
+  # ActionMailer::Base.smtp_settings = if ENV['SMTP_AUTH'].present? && ENV['SMTP_AUTH'] != "none"
+  #   {
+  #     address: ENV['SMTP_SERVER'],
+  #     port: ENV["SMTP_PORT"],
+  #     domain: ENV['SMTP_DOMAIN'],
+  #     user_name: ENV['SMTP_USERNAME'],
+  #     password: ENV['SMTP_PASSWORD'],
+  #     authentication: ENV['SMTP_AUTH'],
+  #     enable_starttls_auto: ENV['SMTP_STARTTLS_AUTO'],
+  #   }
+  # else
+  #   {
+  #     address: ENV['SMTP_SERVER'],
+  #     port: ENV["SMTP_PORT"],
+  #     domain: ENV['SMTP_DOMAIN'],
+  #     enable_starttls_auto: ENV['SMTP_STARTTLS_AUTO'],
+  #   }
+  # end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
